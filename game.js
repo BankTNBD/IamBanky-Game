8c8c3d2d2ac29dca521bebb540b9c1654d655677
Thanabadee Techakasiwattana <bankthanabadee2007@icloud.com>
2021-09-04 11:57:12 +0700

    fix

--- game.js
+++ game.js
@@ -1,32 +1,10 @@
-const deviceType = () => {
-    const ua = navigator.userAgent;
-    if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-        return "tablet";
-    }
-    else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-        return "mobile";
-    }
-    return "desktop";
-};
-
 function init() {
     let stage = new createjs.Stage("gameCanvas");
     let circle = new createjs.Shape();
-
+    
     stage.canvas.width = window.innerWidth;
     stage.canvas.height = window.innerHeight;
-    
-    if (deviceType == 'mobile' || deviceType == 'tablet') {
-        messageField = new createjs.Text("Please Open On Laptop or PC", "bold 250% Arial", "#6cd1ef");
-	    messageField.maxWidth = 1000;
-	    messageField.textAlign = "center";
-	    messageField.textBaseline = "middle";
-	    messageField.x = stage.canvas.width / 2;
-    	messageField.y = stage.canvas.height / 2;
-	    stage.addChild(messageField);
-    }
 
-    else {
     messageField = new createjs.Text("Coming Soon", "bold 550% Arial", "#6cd1ef");
 	messageField.maxWidth = 1000;
 	messageField.textAlign = "center";
@@ -50,5 +28,4 @@ function init() {
 
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", stage);
-    }
 }
